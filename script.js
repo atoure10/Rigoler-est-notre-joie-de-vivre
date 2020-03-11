99867b70d2299b22d31c843dadd16de717c80d2c
Glitch (hello-webpage) <none>
2020-03-11 22:23:52 +0000

    ❣️👨‍❤️‍💋‍👨 Checkpoint ./script.js:1986177/339

--- script.js
+++ script.js
@@ -6,15 +6,22 @@ console.log("hi");
 
 let searchButton = document.querySelector("#search")
 let URL = `https://api.giphy.com/v1/gifs/search?api_key=2a70b934652d4a2dbc908bd10c30b54d&q=`
+let gifContainer = document.querySelector("#gifs")
 
 searchButton.addEventListener("click", getGif)
 
 async function getGif(){
   console.log('I work')
-  let searchTerm = "cat"
+  let searchTerm = document.querySelector("#searchTerm").value
+  
   let response = await fetch(URL+searchTerm)
   console.log(response)
+  
   let gifs = await response.json()
   console.log(gifs)
   
+  let imageAddress = gifs.data[0].images.original.url
+  console.log(imageAddress)
+  
+  gifContainer.innerHTML = `<img src = "${imageAddress}">`
 }
