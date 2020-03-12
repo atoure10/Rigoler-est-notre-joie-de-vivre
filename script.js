18006d6198f263df927c9b403a5ec2c4ed8f8a54
Glitch (hello-webpage) <none>
2020-03-12 19:11:31 +0000

    👼💨 Checkpoint ./script.js:1986177/117

--- script.js
+++ script.js
@@ -6,6 +6,6 @@ searchButton.addEventListener("click", ()=>{
 })
 
 function sendApiRequest(){
-  let response = fetch(`https://api.giphy.com/v1/gifs/search?api_key=YOUR_API_KEY_GOES_HERE&q=Ryan+Gosling&limit=25&offset=0&rating=G&lang=en`)
+  let response = fetch(``)
 }
 
