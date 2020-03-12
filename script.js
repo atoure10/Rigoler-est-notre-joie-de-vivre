ca5c41ca7208f2c97829ff0c560d58ca47a8f8cc
Glitch (hello-webpage) <none>
2020-03-12 16:03:51 +0000

    📅🐯 Checkpoint ./index.html:1986177/11 ./script.js:1986177/1161

--- script.js
+++ script.js
@@ -1,27 +1,11 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
-
-// prints "hi" in the browser's dev tools console
-console.log("hi");
-
 let searchButton = document.querySelector("#search")
-let URL = `https://api.giphy.com/v1/gifs/search?api_key=2a70b934652d4a2dbc908bd10c30b54d&q=`
-let gifContainer = document.querySelector("#gifs")
 
-searchButton.addEventListener("click", getGif)
+searchButton.addEventListener("click", ()=>{
+  console.log("button pressed")
+  sendApiRequest()
+})
 
-async function getGif(){
-  console.log('I work')
-  let searchTerm = document.querySelector("#searchTerm").value
-  
-  let response = await fetch(URL+searchTerm)
-  console.log(response)
-  
-  let gifs = await response.json()
-  console.log(gifs)
-  
-  let imageAddress = gifs.data[0].images.original.url
-  console.log(imageAddress)
-  
-  gifContainer.innerHTML = `<img src = "${imageAddress}">`
+function sendApiRequest(){
+  let response = fetch(`https://api.giphy.com/v1/gifs/search?api_key=YOUR_API_KEY_GOES_HERE&q=Ryan+Gosling&limit=25&offset=0&rating=G&lang=en`)
 }
+
