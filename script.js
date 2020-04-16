3150104eb91da081884a0bf8d0858366633521d6
Glitch (hello-webpage) <none>
2020-04-16 22:24:03 +0000

    🦁⚽️ Checkpoint ./script.js:1986177/507 ./index.html:1986177/7

--- script.js
+++ script.js
@@ -12,13 +12,15 @@ async function sendApiRequest(){
   const API_KEY = "tCDGxK7JcFMa3PWAjCvK7pEHWtXM94vjr2UT87E0"
   let response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`);
   console.log(response)
-  let data = response.json()
+  let data = await response.json()
   console.log(data)
+  useApiData(data)
 }
 
 
 //function that does something with the data received from the API. The name of the function should be customized to whatever you are doing with the data
 function useApiData(data){
-  
+  console.log(data)
+  document.querySelector("#content").innerHTML = `<img src="${data.url}">`
 }
 
