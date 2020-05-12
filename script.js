1283b299d1b4916ee2acdd8dc4fd9b8a3573aa06
Glitch (hello-webpage) <none>
2020-05-12 06:36:56 +0000

    💏👖 Checkpoint ./script.js:10805947/556

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ searchButton.addEventListener("click", ()=>{
 //An asynchronous function to fetch data from the API.
 async function sendApiRequest(){
  
-  let response = await fetch(`https://official-joke-api.appspot.com/jokes/programming/random`);
+  let response = await fetch(`https://sv443.net/jokeapi/v2/joke/Any?idRange=5-185`);
   console.log(response)
   let data = await response.json()
   console.log(data)
@@ -20,7 +20,6 @@ async function sendApiRequest(){
 
 //function that does something with the data received from the API. The name of the function should be customized to whatever you are doing with the data
 function useApiData(data){
-document.querySelector("#content").innerHTML = data[0].punchline
-  document.querySelector("#content").innerHTML = data[0].setup
+document.querySelector("#content").innerHTML = data.joke
 }
 
