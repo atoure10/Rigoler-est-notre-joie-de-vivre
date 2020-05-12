9e07bd76d307564726af2a4083707aa72dd28d26
Glitch (hello-webpage) <none>
2020-05-12 06:48:56 +0000

    🍗🚶 Checkpoint ./script.js:10805947/1364

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ searchButton.addEventListener("click", ()=>{
 //An asynchronous function to fetch data from the API.
 async function sendApiRequest(){
  
-  let response = await fetch(`https://sv443.net/jokeapi/v2/joke/Any?idRange=5-185`);
+  let response = await fetch(`https://official-joke-api.appspot.com/random_ten`);
   console.log(response)
   let data = await response.json()
   console.log(data)
@@ -20,6 +20,31 @@ async function sendApiRequest(){
 
 //function that does something with the data received from the API. The name of the function should be customized to whatever you are doing with the data
 function useApiData(data){
-document.querySelector("#content").innerHTML = data.joke
+document.querySelector("#content").innerHTML += data[0].setup
+  document.querySelector("#content").innerHTML += data[0].punchline
+  
+  document.querySelector("#content").innerHTML += data[1].setup
+  document.querySelector("#content").innerHTML += data[1].punchline
+  
+  document.querySelector("#content").innerHTML += data[2].setup
+  document.querySelector("#content").innerHTML += data[2].punchline
+  
+  document.querySelector("#content").innerHTML += data[3].setup
+  document.querySelector("#content").innerHTML += data[3].punchline
+  
+  document.querySelector("#content").innerHTML += data[4].setup
+  document.querySelector("#content").innerHTML += data[4].punchline
+  
+  document.querySelector("#content").innerHTML += data[5].setup
+  document.querySelector("#content").innerHTML += data[5].punchline
+  
+  document.querySelector("#content").innerHTML += data[6].setup
+  document.querySelector("#content").innerHTML += data[6].punchline
+  
+  document.querySelector("#content").innerHTML += data[7].setup
+  document.querySelector("#content").innerHTML += data[7].punchline
+  
+  document.querySelector("#content").innerHTML += data[8].setup
+  document.querySelector("#content").innerHTML += data[8].punchline
 }
 
